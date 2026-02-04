2190348e030f1e0106ca43e96f538280e1503f7e
Harsh Pendhari <harshpendhari2901@gmail.com>
2026-02-04 22:29:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myhealth/LoginActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myhealth
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.LoginActivity)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val LoginPage_EmailField = findViewById<EditText>(R.id.userId)
+        val LoginPage_PasswordField = findViewById<EditText>(R.id.pwd)
+        val LoginPage_ErrorMSG = findViewById<TextView>(R.id.error_msg)
+        val LoginPage_LoginBTN = findViewById<Button>(R.id.login_btn)
+        val LoginPage_RegisterBTN = findViewById<Button>(R.id.new_registration)
+        val LoginPage_ForgotPasswordBTN = findViewById<Button>(R.id.forgotPassword)
+
+        
+    }
+}
