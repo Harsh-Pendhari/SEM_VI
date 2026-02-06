6b16fb4a42cb0a97c4ceb74d53ef43a0eb3f4198
Harsh Pendhari <harshpendhari2901@gmail.com>
2026-02-06 22:42:57 +0530

    updates

---
+++ app/src/main/java/com/example/myhealth/DashboardActivity.kt
@@ -0,0 +1,51 @@
+package com.example.myhealth
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_dashboard)
+
+        // Handle system insets (status bar, navigation bar)
+        ViewCompat.setOnApplyWindowInsetsListener(
+            findViewById(R.id.DashboardLayout)
+        ) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+
+        // Load default fragment (Home / My Health)
+        if (savedInstanceState == null) {
+            loadFragment(HomeFragment())
+        }
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> loadFragment(HomeFragment())
+                R.id.nav_medicine -> loadFragment(MedicinesFragment())
+                R.id.nav_health -> loadFragment(HealthFragment())
+                R.id.nav_appointment -> loadFragment(AppointmentsFragment())
+                R.id.nav_menu -> loadFragment(MenuFragment())
+            }
+            true
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+}
