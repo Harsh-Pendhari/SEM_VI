347156c9cbf347be669bc1f882cab253a41406b0
Harsh Pendhari <harshpendhari2901@gmail.com>
2026-02-06 22:47:49 +0530

    update

--- app/src/main/java/com/example/myhealth/MainActivity.kt
+++ app/src/main/java/com/example/myhealth/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myhealth
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,5 +17,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        startActivity(Intent(this, DashboardActivity::class.java))
+        finish()
     }
 }
